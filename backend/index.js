3b659a67190607ae402fd15c17a952a8447d0434
wdwithsuraj <surajkrsr98@gmail.com>
2023-08-23 21:28:23 +0530

    day2

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,22 @@
-console.log("Hii")
+const express = require("express")
+const userRoutes = require("./routes/userRoute");
+const connection = require("./config/db")
+
+const app = express();
+app.use(express.json());
+
+
+app.use("/tuneWaves/users", userRoutes);
+
+
+app.get("/", async (req, res) => {
+    res.json({ msg: "Backend For TuneWaves" });
+});
+
+
+app.listen(8080, async () => {
+    await connection
+    console.log("conected to db")
+    console.log("Server is running on port 8080");
+});
+
