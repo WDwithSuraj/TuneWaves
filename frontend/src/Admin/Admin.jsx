7947099e3bc0340c02c22b1e1c4a76fd6c0a7844
Anshul Kushwah <kushwahasg4450@gmail.com>
2023-08-24 23:40:59 +0530

    admin-page

--- frontend/src/Admin/Admin.jsx
+++ frontend/src/Admin/Admin.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from "react";
+import { Crown } from 'lucide-react';
 
 export const Admin = () => {
   return (
-    <div>Admin</div>
-  )
-}
+    <div>
+      <div style={{border:"1px solid",display:"flex",alignItems:"center",justifyContent:"center"}}>
+        <Crown size={50} fill={"#48BB78"} color="#48BB78" />
+      <h1
+        style={{
+          color: "#48BB78",
+          // border: "1px solid",
+          textAlign: "center",
+          width:"8%"
+        }}
+        > 
+        Admin
+        </h1>
+        
+      </div>
+    </div>
+  );
+};
