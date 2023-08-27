5d8eb4cb8e9a94f9d45db4bfd7b783bd98979b7a
rohitnayal12 <rohitnayalkld@gmail.com>
2023-08-27 23:46:50 +0530

    final-commit

--- frontend/src/components/sidebar/index.js
+++ frontend/src/components/sidebar/index.js
@@ -1,38 +1,57 @@
-import React, { useEffect, useState } from 'react'
-import "./sidebar.css"
-import SidebarButton from './sidebarButton';
+import React, { useEffect, useState } from "react";
+import "./sidebar.css";
+import SidebarButton from "./sidebarButton";
 import { MdFavorite } from "react-icons/md";
 import { FaGripfire, FaPlay } from "react-icons/fa";
 import { FaSignOutAlt } from "react-icons/fa";
 import { IoLibrary } from "react-icons/io5";
 import { MdSpaceDashboard } from "react-icons/md";
+import axios from "axios";
+import { Navigate, useNavigate } from "react-router-dom";
 
 function Sidebar() {
-    const [image, setImage] = useState(
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdLAY3C19kL0nV2bI_plU3_YFCtra0dpsYkg&usqp=CAU"
-      );
-    //   useEffect(() => {
-    //     apiClient.get("me").then((response) => {
-    //       setImage(response.data.images[0].url);
-    //     });
-    //   }, []);
+
+  const navigate=useNavigate()
+  const [image, setImage] = useState(
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdLAY3C19kL0nV2bI_plU3_YFCtra0dpsYkg&usqp=CAU"
+  );
+    const token = localStorage.getItem("token")
+
+    const logOut = () => {
+      axios(`http://localhost:8080/tuneWaves/users/logout`,{
+        method : "POST",
+        headers : {
+          Authorization : `Bearer ${token}`
+        },
+
+      }).then((res)=>{
+        localStorage.clear()
+        alert("You have Logged out successfully.")
+        
+        navigate("/")
+
+      })
+      
+    }
+
   return (
     <div className="sidebar-container">
       <img src={image} className="profile-img" alt="profile" />
       <div>
-        <SidebarButton title="Feed" to="/feed" icon={<MdSpaceDashboard />} />
-        <SidebarButton title="Trending" to="/trending" icon={<FaGripfire />} />
+        <SidebarButton title="Library" to="/" icon={<IoLibrary />} />
         <SidebarButton title="Player" to="/player" icon={<FaPlay />} />
         <SidebarButton
           title="Favorites"
           to="/favorites"
           icon={<MdFavorite />}
         />
-        <SidebarButton title="Library" to="/" icon={<IoLibrary />} />
+        <SidebarButton title="Feed" to="/feed" icon={<MdSpaceDashboard />} />
+        <SidebarButton title="Trending" to="/trending" icon={<FaGripfire />} />
+        
       </div>
-      <SidebarButton title="Sign Out" to="" icon={<FaSignOutAlt />} />
+      <SidebarButton title="Sign Out" to=""  icon={<FaSignOutAlt onClick={()=> logOut()}/>} />
     </div>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
--- frontend/src/screens/favourites/index.js
+++ frontend/src/screens/favourites/index.js
@@ -1,8 +1,81 @@
-import React from 'react'
+// import React from 'react'
+
+// function Favorite() {
+//   return (
+//     <div className="screen-container">Favorites</div>
+//   )
+// }
+
+// export default Favorite
+
+
+
+import React, { useEffect, useState } from 'react'
+import { IconContext } from "react-icons";
+import "./favorite.css";
+import { AiFillPlayCircle } from "react-icons/ai";
+import {  useNavigate } from "react-router-dom";
 
 function Favorite() {
+
+  const [likesongs,setLikesongs]=useState([])
+  const token = localStorage.getItem("token");
+  console.log(token);
+
+  const fetchdata=()=>{
+    fetch("http://localhost:8080/tuneWaves/songs/like", {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data)
+        setLikesongs(data.data)
+        
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  useEffect(()=>{
+    fetchdata()
+  },[])
+
+  console.log(likesongs)
+
+  const playPlaylist = (index) => {
+    navigate("/player", { state: { index, likesongs } });
+  };
+
+  const navigate = useNavigate();
+
   return (
-    <div className="screen-container">Favorites</div>
+    <div className="screen-container">
+      <div className="library-body">
+        {likesongs?.map((playlist, index) => (
+          <div
+            className="playlist-card"
+            key={playlist._id}
+          >
+            <img
+              src={playlist.image}
+              className="playlist-image"
+              alt="Playlist-Art"
+            />
+            <p className="playlist-title">{playlist.title}</p>
+            <p className="playlist-artist">{playlist.artist} </p>
+            
+           
+            
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
